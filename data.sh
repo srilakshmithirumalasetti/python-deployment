380c965e5b21893ee40257ac841ee9e438801a66
lavanyasomepalli <lavanyasomepalli164@gmail.com>
2024-12-24 23:47:39 +0530

    Update data.sh

--- data.sh
+++ data.sh
@@ -1,15 +1,7 @@
 #!/bin/bash
-
-sudo yum upgrade -y
-
 sudo yum install git -y
-
 git clone https://github.com/lavanyasomepalli/flight-perdiction.git
-
 mv flight-prediction /home/ec2-user/
-
-cd /home/ec2-user/flight-prediction
-
+cd /home/ec2-user/flight-perdiction
 pip3 install -r requirements.txt
-
 python3 app.py

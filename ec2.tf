2fbb349c61c752bd94c933bf81eb79eeaf583b8d
Bhargavi Bairagoni <113205911+bhargavibairagoni@users.noreply.github.com>
2024-12-24 16:22:34 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -16,30 +16,9 @@ resource "aws_security_group" "demosg" {
 vpc_id="${aws_vpc.demovpc.id}"
 
 ingress{
-from_port=80
-to_port=80
-protocol="tcp"
-cidr_blocks=["0.0.0.0/0"]
-}
-
-ingress{
-from_port=8080
-to_port=8080
-protocol="tcp"
-cidr_blocks=["0.0.0.0/0"]
-}
-
-ingress{
-from_port=7000
-to_port=7000
-protocol="tcp"
-cidr_blocks=["0.0.0.0/0"]
-}
-
-ingress{
-from_port=22
-to_port=22
-protocol="tcp"
+from_port=0
+to_port=0
+protocol="-1"
 cidr_blocks=["0.0.0.0/0"]
 }
 

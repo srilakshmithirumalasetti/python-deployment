eb075c551e0c74f8ee31ec2213f01a8b9f6bcc5d
Bhargavi Bairagoni <113205911+bhargavibairagoni@users.noreply.github.com>
2024-12-24 16:48:51 +0530

    Update data.sh

--- data.sh
+++ data.sh
@@ -5,7 +5,7 @@ sudo apt install python3-pip -y
 sudo apt install pip3 -y
 mkdir /bhargavi
 cd /home/ubuntu/
-git clone https://github.com/bhargavibairagoni/USA-Housing.git
-cd /home/ubuntu/USA-Housing
+git clone https://github.com/bhargavibairagoni/indian-liver-patients.git
+cd /indian-liver-patients
 pip3 install -r requirements.txt
 screen -m -d python3 ./app.py

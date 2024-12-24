371b531dd714eca79712e0b7b6f03fdba6f1faf0
lavanyasomepalli <lavanyasomepalli164@gmail.com>
2024-12-24 22:46:16 +0530

    Update data.sh

--- data.sh
+++ data.sh
@@ -6,9 +6,11 @@ sudo yum install git -y
 
 sudo yum install -y python3 python3-pip git
 
-git clone https://github.com/srilakshmithirumalasetti/car-prediction.git
+git clone https://github.com/lavanyasomepalli/flight-perdiction.git
 
-cd /root/car-prediction
+mv flight-prediction/home/ec2-user/
+
+cd /home/ec2-user/flight-prediction
 
 pip3 install -r requirements.txt
 

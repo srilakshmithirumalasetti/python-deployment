0e299afe959dfc6ea7486e386e092d69353156a2
lavanyasomepalli <lavanyasomepalli164@gmail.com>
2024-12-24 22:28:15 +0530

    Update data.sh

--- data.sh
+++ data.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
-sudo apt update -y
-sudo apt install git -y
-sudo apt install python3-pip -y
-sudo apt install pip3 -y
-mkdir /bhargavi
-cd /home/ubuntu/
-git clone https://github.com/bhargavibairagoni/indian-liver-patients.git
-cd /indian-liver-patients
+
+sudo yum upgrade -y
+
+sudo yum install git -y
+
+sudo yum install -y python3 python3-pip git
+
+git clone https://github.com/srilakshmithirumalasetti/car-prediction.git
+
+cd /root/car-prediction
+
 pip3 install -r requirements.txt
-screen -m -d python3 ./app.py
+
+nohup python3 app.py &

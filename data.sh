e1fd476ce6d09a208c9aeb37566c2f35bf505c32
srilakshmithirumalasetti <srilakshmithirumalasetti2@gmail.com>
2024-12-25 21:49:19 +0530

    Update data.sh

--- data.sh
+++ data.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo yum install git -y
-sudo git clone https://github.com/srilakshmithirumalasetti/hearing.git
-sudo mv hearing/ /home/ec2-user/
-cd /home/ec2-user/hearing
+sudo git clone https://github.com/srilakshmithirumalasetti/Penguin-.git
+sudo mv Penguin-/ /home/ec2-user/
+cd /home/ec2-user/Penguin-
 pip3 install -r requirements.txt
 screen -m -d python3 app.py

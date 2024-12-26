eba914005e080a9e1c0a8d7cf3b335a7182def2d
srilakshmithirumalasetti <srilakshmithirumalasetti2@gmail.com>
2024-12-26 16:25:35 +0530

    Update data.sh

--- data.sh
+++ data.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo yum install git -y
-sudo git clone https://github.com/srilakshmithirumalasetti/Penguin-.git
-sudo mv Penguin-/ /home/ec2-user/
-cd /home/ec2-user/Penguin-
+sudo git clone https://github.com/srilakshmithirumalasetti/Medical-Insurance.git
+sudo mv Medical-Insurance/ /home/ec2-user/
+cd /home/ec2-user/Medical-Insurance
 pip3 install -r requirements.txt
 screen -m -d python3 app.py

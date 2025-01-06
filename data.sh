afdcd37d10a1e40370b1ef32d8026286e21ed66d
srilakshmithirumalasetti <srilakshmithirumalasetti2@gmail.com>
2025-01-06 21:23:14 +0530

    Update data.sh

--- data.sh
+++ data.sh
@@ -2,7 +2,7 @@
 sudo yum update -y
 sudo yum install git -y
 git clone https://github.com/srilakshmithirumalasetti/indian-liver-patients.git
-sudo mv indian-liver-patients/ /home/ec2-user/
+sudo mv indian-liver-patients /home/ec2-user/
 cd /home/ec2-user/indian-liver-patients
 sudo yum install python3 -y
 yum install python3-pip -y
